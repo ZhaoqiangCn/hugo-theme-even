1ba51cb12824ad00fbc4de4a59f65cee6a118719
zhaoqiangcn <zq19890000@gmail.com>
2020-02-12 17:11:53 +0800

    rebuilding site 2020年02月12日 17:11:53

--- static/js/search.js
+++ static/js/search.js
@@ -1,4 +1,4 @@
-(function() {
+$(function() {
   // 替换成自己的algolia信息
   var client = algoliasearch("EO42BFKWB5","acbdc102e2c95f016d423fa03690923d");
   var index = client.initIndex("www.nashome.cn");
@@ -31,7 +31,7 @@
   );
 
 
-  (document).on("click", ".aa-suggestion", function () { 
+  $(document).on("click", ".aa-suggestion", function () { 
     var aa = $(this).find("a").attr("href");
     window.location.href = aa;
   })
